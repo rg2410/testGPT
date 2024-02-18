dd56594c2eef5308270d86f32418a88426ea6ea2
rg2410 <rafaelgomesfernandes@hotmail.com>
2024-02-18 18:22:14 -0300

    refactored train function

--- train.py
+++ train.py
@@ -4,27 +4,26 @@ training script
 
 import torch
 
-def get_batch(data, batch_size, config):
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+def get_batch(data, batch_size, block_size):
     # generate a small batch of data of inputs x and targets y
-    ix = torch.randint(len(data) - config.block_size, (batch_size,))
-    x = torch.stack([data[i:i+config.block_size] for i in ix])
-    y = torch.stack([data[i+1:i+config.block_size+1] for i in ix])
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
     x, y = x.to(device), y.to(device)
     return x, y
 
 @torch.no_grad()
-def estimate_loss(model, eval_iters, training_data, val_data, batch_size, config):
+def estimate_loss(model, eval_iters, train_data, val_data, batch_size, block_size):
     out = {}
     model.eval()
     for split in ['train', 'val']:
         losses = torch.zeros(eval_iters)
         for k in range(eval_iters):
-            if split == 'train':
-                data = training_data
-            else:
-                data = val_data
-            X, Y = get_batch(data, batch_size, config)
+            data = train_data if split == 'train' else val_data
+            X, Y = get_batch(data, batch_size, block_size)
             logits, loss = model(X, Y)
             losses[k] = loss.item()
         out[split] = losses.mean()
